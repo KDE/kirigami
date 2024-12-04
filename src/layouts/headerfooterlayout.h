c42fe0d1dd81a1c722f3c6c960071b8bbb709b8a
Nicolas Fella <nicolas.fella@gmx.de>
2024-12-04 20:41:53 +0100

    Port API documentation syntax to QDoc

--- src/layouts/headerfooterlayout.h
+++ src/layouts/headerfooterlayout.h
@@ -10,36 +10,45 @@
 #include <QQuickItem>
 #include <qtmetamacros.h>
 
-/**
- * replicates a little part of what Page does,
+/*!
+ * \qmltype HeaderFooterLayout
+ * \inqmlmodule org.kde.kirigami.layouts
+ *
+ * \brief Replicates a little part of what Page does.
+ *
  * It's a container with 3 properties, header, contentItem and footer
- * which will be laid out oone on top of each other. It works better than a
- * ColumnLayout when the elements are to be defined by properties by the
- * user, which would require ugly reparenting dances and container items to
+ * which will be laid out oone on top of each other.
+ *
+ * It works better than a ColumnLayout when the elements are to be
+ * defined by properties by the user, which would require ugly
+ * reparenting dances and container items to
  * maintain the layout well behaving.
  */
 class HeaderFooterLayout : public QQuickItem
 {
     Q_OBJECT
     QML_ELEMENT
-    /**
-     * @brief This property holds the page header item.
+    /*!
+     * \qmlproperty Item HeaderFooterLayout::header
+     * \brief This property holds the page header item.
      *
      * The header item is positioned to the top,
      * and resized to the width of the page. The default value is null.
      */
     Q_PROPERTY(QQuickItem *header READ header WRITE setHeader NOTIFY headerChanged FINAL)
 
-    /**
-     * @brief This property holds the visual content Item.
+    /*!
+     * \qmlproperty Item HeaderFooterLayout::contentItem
+     * \brief This property holds the visual content Item.
      *
      * It will be resized both in width and height with the layout resizing.
      * Its height will be resized to still have room for the heder and footer
      */
     Q_PROPERTY(QQuickItem *contentItem READ contentItem WRITE setContentItem NOTIFY contentItemChanged FINAL)
 
-    /**
-     * @brief This property holds the page footer item.
+    /*!
+     * \qmlproperty Item HeaderFooterLayout::footer
+     * \brief This property holds the page footer item.
      *
      * The footer item is positioned to the bottom,
      * and resized to the width of the page. The default value is null.
@@ -59,8 +68,9 @@ public:
     void setFooter(QQuickItem *item);
     QQuickItem *footer();
 
-    /**
-     * @brief HeaderFooterLayout normally positions its header, footer and
+    /*!
+     * \qmlmethod void HeaderFooterLayout::forceLayout()
+     * \brief HeaderFooterLayout normally positions its header, footer and
      * contentItem once per frame (at polish event). This method forces the it
      * to recalculate the layout immediately.
      */
