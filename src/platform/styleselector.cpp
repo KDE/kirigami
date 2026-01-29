58202a0780505225fc2bedb0eb2bcdc9dc98a5d1
Marco Martin <notmart@gmail.com>
2026-01-29 17:14:36 +0100

    Styles work again
    Make styles work again, keeping existing styles compatible,
    so both styles and icons are installed in the same folder.
    
    They will need to be ported to proper imports, but for now let's keep
    them where they used to be to not have to do 2 ports in a short time

--- src/platform/styleselector.cpp
+++ src/platform/styleselector.cpp
@@ -40,7 +40,7 @@ QStringList StyleSelector::styleChain()
 #if !defined(Q_OS_ANDROID) && !defined(Q_OS_IOS)
     // org.kde.desktop.plasma is a couple of files that fall back to desktop by purpose
     if (style.isEmpty() || style == QStringLiteral("org.kde.desktop.plasma")) {
-        auto path = resolveFilePath(QStringLiteral("/styles/org.kde.desktop"));
+        auto path = resolveFilePath(QStringLiteral("/../styles/org.kde.desktop"));
         if (QFile::exists(path)) {
             s_styleChain.prepend(QStringLiteral("org.kde.desktop"));
         }
@@ -51,11 +51,11 @@ QStringList StyleSelector::styleChain()
     s_styleChain.prepend(QStringLiteral("Material"));
 #endif
 
-    auto stylePath = resolveFilePath(QStringLiteral("/styles/") + style);
+    auto stylePath = resolveFilePath(QStringLiteral("/../styles/") + style);
     if (!style.isEmpty() && QFile::exists(stylePath) && !s_styleChain.contains(style)) {
         s_styleChain.prepend(style);
         // if we have plasma deps installed, use them for extra integration
-        auto plasmaPath = resolveFilePath(QStringLiteral("/styles/org.kde.desktop.plasma"));
+        auto plasmaPath = resolveFilePath(QStringLiteral("/../styles/org.kde.desktop.plasma"));
         if (style == QStringLiteral("org.kde.desktop") && QFile::exists(plasmaPath)) {
             s_styleChain.prepend(QStringLiteral("org.kde.desktop.plasma"));
         }
@@ -72,7 +72,7 @@ QUrl StyleSelector::componentUrl(const QString &fileName)
 {
     const auto chain = styleChain();
     for (const QString &style : chain) {
-        const QString candidate = QStringLiteral("styles/") + style + QLatin1Char('/') + fileName;
+        const QString candidate = QStringLiteral("../styles/") + style + QLatin1Char('/') + fileName;
         if (QFile::exists(resolveFilePath(candidate))) {
             return QUrl(resolveFileUrl(candidate));
         }
